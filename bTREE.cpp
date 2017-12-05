a2565dd6725ff494dc23b80d217e6bea90b7f6dd
uictor <31808074+uictor@users.noreply.github.com>
2017-12-04 20:18:37 -0700

    Add files via upload

--- bTREE.cpp
+++ bTREE.cpp
@@ -1,43 +1,237 @@
 #include "bTREE.h"
-
+#include <vector>
+#include <iostream>
+using namespace std;
 
 //look at descriptions in pMT.h for guidance on what you might need for these function to actually do
 bTREE::bTREE()
 {
+	rootNode = new treeNode();
+	rootNode->data = "";
+	rootNode->time = 0;
+	rootNode->isLeaf = true;
+	lTree = NULL;
+	rTree = NULL;
+    myQueue.push(this);
+	myQueueSize++;
 }
 
-bTREE::~bTREE()
-{
+bTREE::~bTREE(){
+	delete rootNode;
 }
 
 int bTREE::dataInserted()
 {
+	int numData = 0;
+	if (rootNode == NULL){
+		numData= 0;
+	}
+	else if (rootNode->isLeaf == true){
+		numData = 1;
+	}
+	else{
+		numData = lTree->dataInserted() + rTree->dataInserted();
+	}
+	return numData;
 }
 
-int bTREE::numberOfNodes()
-{
+int bTREE::numberOfNodes(){
+	int numNodes = 0;
+	if (rootNode == NULL)
+	{
+		numNodes = 0;
+	}
+	else if (lTree->rootNode == NULL && rTree->rootNode == NULL)
+	{
+		numNodes = 1;
+	}
+	else
+	{
+		numNodes = 1 + lTree->numberOfNodes() + rTree->numberOfNodes();
+	}
+	return numNodes;
 }
 
-int bTREE::insert(string data, int time)
+
+bTREE* bTREE::getLeftTree() const
 {
+    return lTree;
+}
+bTREE* bTREE::getRightTree() const
+{
+    return rTree;
 }
 
-int bTREE::find(string)
+int bTREE::insert(string data1, int time1)
 {
+	static int noOfSteps = 0;
+	bool insert = false;
+	bTREE * newTree = new bTREE();
+	treeNode* newNode = new treeNode();
+	newNode->data = data1;
+	newNode->time = time1;
+	newNode->isLeaf = true;
+	newTree->rootNode = newNode;
+	newTree->lTree = NULL;
+	newTree->rTree = NULL;
+
+	if (rootNode == NULL){
+		rootNode = newNode;
+		myQueue.push(newTree);
+		myQueueSize++;
+		noOfSteps++;
+		insert = true;
+	}
+
+	else if(myQueue.front()->lTree == NULL) {
+		myQueue.front()->lTree = newTree;
+		myQueue.push(newTree);
+		myQueueSize++;
+		noOfSteps+=2;
+		rootNode->isLeaf = false;
+		insert = true;
+		
+	}
+
+	else if (myQueue.front()->rTree == NULL)
+	{
+		myQueue.front()->rTree = newTree;
+		myQueue.push(newTree);
+		myQueueSize++;
+		myQueue.pop();
+		myQueueSize--;
+		noOfSteps +=3;
+		rootNode->isLeaf = false;
+		insert = true;
+		
+	}
+	if(insert)
+	{
+		
+		return noOfSteps;
+	}
+	return 0;
 }
 
-string bTREE::locate()
+int bTREE::find(string x)
 {
+    static bool found = false;
+    static int steps= 0;
+    if((rootNode->data != x) && (rootNode->isLeaf == false))
+    {
+        steps = 1 + rTree->find(x)+lTree->find(x);
+    }
+    else
+    {
+       steps ++;
+       found = true;
+    }
+    if(found)
+    {
+        return steps;
+    }
+    return 0;
 }
 
-friend bool bTREE::operator ==(const bTREE& lhs, const bTREE& rhs)
+string bTREE::locate(string x)
 {
+    static string directions;
+	cout << "test:" << find(x) << endl;
+    if(find(x)==0)
+    {
+        directions = ".";
+    }
+    else
+    {
+        if((rootNode->data == x) && (rootNode->isLeaf == false))
+        {
+            directions = directions+(" ");
+        }
+		else if((lTree->find(x) > 0) && (rootNode->isLeaf == false)){
+                directions = directions + "L";
+                lTree->locate(x);
+            }
+		else if ((rTree->find(x)) > 0 && (rootNode->isLeaf == false))
+		{
+			directions = directions + "R";
+			rTree->locate(x);
+		}
+    }
+    return directions;
 }
 
-friend bool bTREE::operator !=(const bTREE& lhs, const bTREE& rhs)
+bool operator ==(const bTREE& lhs, const bTREE& rhs)
 {
+    if(lhs != rhs)
+    {
+        return false;
+    }
+    return true;
 }
 
-friend std::ostream& bTREE::operator <<(std::ostream& out, const bTREE& p)
+bool operator!=(const bTREE & lhs, const bTREE & rhs)
 {
+    static bool not_equality = false;
+    if(lhs.rootNode->data != rhs.rootNode->data)
+    {
+        not_equality = true;
+    }
+    else if((lhs.lTree != rhs.lTree) || (lhs.rTree != rhs.rTree))
+    {
+        not_equality = true;
+    }
+    return not_equality;
 }
+
+void
+   bTREE:: displayLeft( std::ostream & outfile,
+   bTREE * subtree, std::string prefix )
+{
+   if( subtree == NULL )
+   {
+      outfile << prefix + "/" << endl;
+   }
+   else
+   {
+      displayLeft( outfile, subtree->lTree, prefix + "     " );
+	  outfile << prefix + "\\---" << subtree->rootNode->data << std::endl;
+      displayRight( outfile, subtree->rTree, prefix + "|    " );
+   }
+}
+
+
+void
+   bTREE:: displayRight( std::ostream & outfile,
+   bTREE * subtree, std::string prefix )
+{
+   if( subtree == NULL )
+   {
+      outfile << prefix + " " << std::endl;
+   }
+   else
+   {
+      displayLeft( outfile, subtree->lTree, prefix + "|    " );
+      
+	  outfile << prefix + "  " << subtree->rootNode->data << std::endl;
+      displayRight( outfile, subtree->rTree, prefix + "     " );
+   }
+}
+
+
+std::ostream& operator <<(std::ostream& out, const bTREE* p)
+{
+   std::string prefix;
+   if( p->rootNode->isLeaf == true)
+   {
+      out << "- " << endl;
+   }
+   else
+   {
+      bTREE::displayLeft( out, p->lTree, "    " );
+	  out << " " << p->rootNode->data << std::endl;
+
+      bTREE::displayRight( out, p->rTree, "    " );
+   }
+   return out;
+}
+
--- pMT.cpp
+++ pMT.cpp
@@ -1,11 +1,15 @@
-
+#include "pMT.h"
+#include <cstring>
+#include<stdlib.h>
 pMT::pMT(int hashSelect)
 /**
- * @brief 
+ * @brief
  * @param hashSelect a number corresponding to the hashfunction to use for this pMT
- * @return 
+ * @return
  */
 {
+	myMerkle = new bTREE();
+    selectedHash = hashSelect;
 }
 
 pMT::~pMT()
@@ -20,11 +24,12 @@ int pMT::insert(string vote, int time)
 /**
  * @brief insert a vote and time into a leaf node of tree
  * @param vote - a string
- * @param time - an int representing the time 
+ * @param time - an int representing the time
  * @return the number of operations needed to do the insert, -1 if out of memory
  */
 
 {
+    return myMerkle->insert(vote, time);
 }
 
 int pMT::find(string vote, int time, int hashSelect)
@@ -33,10 +38,24 @@ int pMT::find(string vote, int time, int hashSelect)
  * @param vote, a string
  * @param time, an int
  * @param hashSelect, an int corresponding to the hash functions _1, _2, and _3
- * @return 0 if not found, else number of opperations required to find the matching vote
+ * @return 0 if not found, else number of operations required to find the matching vote
  */
 {
-    
+    string Hash;
+    if(hashSelect<=1)
+    {
+        Hash = hash_1(vote);
+    }
+    else if(hashSelect==2)
+    {
+        Hash = hash_2(vote);
+    }
+    else if(hashSelect>=3)
+    {
+        Hash = hash_3(vote);
+    }
+    return myMerkle->find(Hash);
+
 }
 
 int pMT::findHash(string mhash)
@@ -46,28 +65,33 @@ int pMT::findHash(string mhash)
  * @return 0 if not found, else number of opperations required to find the matching hash
  */
 {
+    return myMerkle->find(mhash);
+}
+//Created one to locate both data and hash
+string pMT:: locate(string dathash)
+{
+    return myMerkle->locate(dathash);
 }
 
-
-string pMT::locateData(string vote)
+//string pMT::locateData(string vote)
 /**
- * @brief Function takes a hash of Vote and returns the sequence of (L)eft and (R)ight moves to get to that node starting from root. 
- * @param vote, the data to search for 
+ * @brief Function takes a hash of Vote and returns the sequence of (L)eft and (R)ight moves to get to that node starting from root.
+ * @param vote, the data to search for
  * @return sequence of L's and R's comprising the movement to the leaf node; else return a dot '.'
  */
-{
-}
+//{
+//}
 
-string pMT::locateHash(string mhash)
+//string pMT::locateHash(string mhash)
 /**
- * @brief Function takes a hash and returns the sequence of (L)eft and (R)ight moves to get to that node starting from root. 
- * @param mhash, the hash to search for 
+ * @brief Function takes a hash and returns the sequence of (L)eft and (R)ight moves to get to that node starting from root.
+ * @param mhash, the hash to search for
  * @return sequence of L's and R's comprising the movement to the hash node, ; else return a dot '.'
  */
-{
-}
+//{
+//}
 
-
+// I could only come up with two hash functions myself
 
 string pMT::hash_1(string key)
 /**
@@ -76,6 +100,20 @@ string pMT::hash_1(string key)
  * @return a hash of the key
  */
 {
+    char hashStr[32];
+    char* str = new char[32];
+    strcpy(str, key.c_str());
+    unsigned int len = key.length();
+    unsigned int seed = 4231;
+    unsigned int hash = 0;
+    unsigned int i    = 0;
+
+    for (i = 0; i < len; ++str, ++i){
+       hash = (hash * seed)*(hash*seed) + (*str) +(hash >> 10); 
+    }
+    itoa(hash, hashStr, 16);
+    return hashStr;
+	//hashStr is the hexadecimal hash 
 }
 
 string pMT::hash_2(string key)
@@ -85,6 +123,7 @@ string pMT::hash_2(string key)
  * @return a hash of the key
  */
 {
+	return "";
 }
 
 string pMT::hash_3(string key)
@@ -94,9 +133,20 @@ string pMT::hash_3(string key)
  * @return a hash of the key
  */
 {
+		unsigned int hash = 0xAAAAAAAA;
+		unsigned int i = 0;
+		int len = key.length();
+		const char* str = key.c_str();
+		for (i = 0; i < len; ++str, ++i)
+		{
+			hash ^= ((i & 1) == 0) ? (~(hash << 1) * (hash >> 5) ^(*str)) :
+				(~((hash << 2) *((*str) + ~(hash >> 7))));
+		}
+
+		return std::to_string(hash);
 }
 
-friend bool pMT::operator ==(const pMT& lhs, const pMT& rhs)
+bool operator ==(const pMT& lhs, const pMT& rhs)
 /**
  * @brief Comparison between two merkle trees
  * @param lhs, the left hand side of the equality statment
@@ -104,9 +154,14 @@ friend bool pMT::operator ==(const pMT& lhs, const pMT& rhs)
  * @return true if equal, false otherwise
  */
 {
+    if(lhs.myMerkle->rootNode->data == rhs.myMerkle->rootNode->data)
+    {
+        return true;
+    }
+    return false;
 }
 
-friend bool pMT::operator !=(const pMT& lhs, const pMT& rhs)
+bool operator !=(const pMT& lhs, const pMT& rhs)
 /**
  * @brief Comparison between two merkle trees
  * @param lhs, the left hand side of the equality statment
@@ -114,38 +169,121 @@ friend bool pMT::operator !=(const pMT& lhs, const pMT& rhs)
  * @return true if not equal, false otherwise
  */
 {
-    
+    if(lhs == rhs)
+    {
+        return false;
+    }
+    return true;
 }
 
-friend pMT pMT::operator ^=(const pMT& lhs, const pMT& rhs)
-/**
- * @brief XOR between two merkle trees
- * @param lhs, the left hand side of the equality statment
- * @param rhs, the right hand side of the equality statement
- * @return true if not equal, false otherwise
- */
-{
-    
-}
-
-
-friend std::ostream& pMT::operator <<(std::ostream& out, const pMT& p)
-/**
- * @brief Print out a tree
- * @param out
- * @param p
- * @return a tree to the screen
- */
-{
-}
-
-
-friend pMT pMT::operator ^(const pMT& lhs, const pMT& rhs)
+pMT operator ^(const pMT& lhs, const pMT& rhs)
 /**
  * @brief Where do two trees differ
  * @param lhs
  * @param rhs
- * @return a tree comprised of the right hand side tree nodes that are different from the left
+ * @return a tree comprised of the right hand side tree nodes that are NOT different from the left
  */
 {
+    if((lhs==rhs) && (lhs.myMerkle->isRootNodeLeaf() == false) && (rhs.myMerkle->isRootNodeLeaf() == false))
+    {
+        return lhs;
+    }
+    else
+    {
+        pMT tmp(lhs.selectedHash);
+        pMT tmp2(lhs.selectedHash);
+        pMT LLpmt(lhs.selectedHash);
+        LLpmt.myMerkle = lhs.myMerkle->getLeftTree();
+        pMT LRpmt(lhs.selectedHash);
+        LRpmt.myMerkle = lhs.myMerkle->getLeftTree();
+        pMT RLpmt(lhs.selectedHash);
+        RLpmt.myMerkle = lhs.myMerkle->getLeftTree();
+        pMT RRpmt(lhs.selectedHash);
+        RRpmt.myMerkle = lhs.myMerkle->getLeftTree();
+        tmp = LLpmt^RLpmt;
+        tmp2 = LRpmt^RRpmt;
+        if(tmp!= NULL)
+        {
+            return tmp;
+        }
+        else if(tmp2!=NULL)
+        {
+            return tmp2;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+}
+
+
+//std::ostream& operator <<(std::ostream& out, const pMT& p)
+///**
+// * @brief Print out a tree
+// * @param out
+// * @param p
+// * @return a tree to the screen
+// */
+//{
+//}
+
+
+
+std::ostream& operator <<(std::ostream& out, const pMT& p)
+{
+	std::string prefix;
+	if (p.myMerkle->rootNode->isLeaf == true)
+	{
+		out << "-" << endl;
+	}
+	else
+	{
+		bTREE::displayLeft(out, p.myMerkle->lTree, "    ");
+		
+		out << "---" << p.myMerkle->rootNode->data << std::endl;
+
+		bTREE::displayRight(out, p.myMerkle->rTree, "    ");
+	}
+	return out;
+}
+
+string pMT::hash_selected(string data)
+{
+	switch (selectedHash)
+	{
+		case 1: return hash_1(data);
+		break;
+		case 2: return hash_2(data);
+		break;
+		case 3: return hash_3(data);
+		default:
+		return hash_1(data);
+	}
+}
+
+void pMT::rhash(bTREE *tree)
+{
+	int noOfSteps = 0;
+	if((tree->lTree->lTree != NULL) && (tree->lTree->rTree != NULL))
+	{
+		rhash(tree->lTree);
+		noOfSteps++;
+	}
+
+	if((tree->rTree->lTree != NULL) && (tree->rTree->rTree != NULL))
+	{
+		rhash(tree->rTree);
+		noOfSteps++;
+	}
+
+	tree->rootNode->data = hash_selected(hash_selected(tree->lTree->rootNode->data) + hash_selected(tree->rTree->rootNode->data));
+	noOfSteps++;
+	//return number of Steps;
+}
+
+void pMT::update()
+{
+	rhash(myMerkle);
 }
